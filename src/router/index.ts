9f60284a78c624e2df0c1d0ff253841a55b33c0b
WALKER <woodpk@sesc.bz>
2026-09-15 15:08:14 +0900

    GitHub F5対策 ルートに#

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,12 +5,13 @@
  */
 
 // Composables
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
 import index from '@/pages/index.vue'
 import gasket from '@/pages/gasket.vue';
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    //history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHashHistory(import.meta.env.BASE_URL), 
     routes: [
         { path: '/', component: index, meta: { title: 'シェルピンスキー・カーペット' } },
         { path: '/gasket', component: gasket, meta: { title: 'シェルビンスキー・ガスケット' } },
